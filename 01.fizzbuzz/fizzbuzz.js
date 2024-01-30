039fc7c0de74a3c8561684409dacb08641e3a740
yuyanishimura <yuya.nishimura@medpeer.co.jp>
2024-01-30 16:59:40 +0900

    不要な処理の削除

--- 01.fizzbuzz/fizzbuzz.js
+++ 01.fizzbuzz/fizzbuzz.js
@@ -1,6 +1,5 @@
 for (let i = 1; i <= 20; i++) {
   let output = '';
-  if (i % 5 === 0 && i % 3 === 0) output += 'FizzBuzz';
   if (i % 3 === 0) output += 'Fizz';
   if (i % 5 === 0) output += 'Buzz';
   console.log(output || i);
