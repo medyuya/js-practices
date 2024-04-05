3e081f90ee1c425dd7fb88d7ca9c7a7abe8457cf
yuyanishimura <yuya.nishimura@medpeer.co.jp>
2024-04-05 10:12:49 +0900

    月内の日付を順番に表示する部分の作成

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -1,4 +1,4 @@
-import { format } from "date-fns";
+import { endOfMonth, format, getDay, isSaturday } from "date-fns";
 // ESMでCommonJSモジュールをインポートするため
 import { createRequire } from "module";
 const require = createRequire(import.meta.url);
@@ -15,3 +15,21 @@ const targetMonthName = format(targetDate, "MMMM");
 
 console.log(`     ${targetMonthName} ${targetYear}`);
 console.log("Su Mo Tu We Th Fr Sa");
+
+const lastDayOfMonth = endOfMonth(targetDate).getDate();
+
+const firstWeekBlank = "   ".repeat(getDay(targetDate));
+
+let date_num = firstWeekBlank;
+let isLastDayOfWeek;
+for (let i = 1; i <= lastDayOfMonth; i++) {
+  if (i.toString().length < 2) date_num += " ";
+  date_num += i.toString() + " ";
+
+  isLastDayOfWeek = isSaturday(new Date(targetYear, targetMonth - 1, i));
+
+  if (isLastDayOfWeek || i == lastDayOfMonth) {
+    console.log(date_num);
+    date_num = "";
+  }
+}
