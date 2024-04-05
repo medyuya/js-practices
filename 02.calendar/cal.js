66ea333cf20c56dcec13739b318af38b4f15321e
yuyanishimura <yuya.nishimura@medpeer.co.jp>
2024-04-05 10:10:33 +0900

    カレンダーのヘッダー部分を表示する処理の作成

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -1,3 +1,4 @@
+import { format } from "date-fns";
 // ESMでCommonJSモジュールをインポートするため
 import { createRequire } from "module";
 const require = createRequire(import.meta.url);
@@ -10,4 +11,7 @@ const targetYear = args.y ?? today.getFullYear();
 const targetMonth = args.m ?? today.getMonth() + 1;
 const targetDate = new Date(targetYear, targetMonth - 1);
 
-console.log(targetDate);
+const targetMonthName = format(targetDate, "MMMM");
+
+console.log(`     ${targetMonthName} ${targetYear}`);
+console.log("Su Mo Tu We Th Fr Sa");
