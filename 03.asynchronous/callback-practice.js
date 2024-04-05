2827b6eb30383584fd896ddb370c634a3448967b
yuyanishimura <yuya.nishimura@medpeer.co.jp>
2024-04-05 17:11:14 +0900

    node-sqlite3のコールバック形式を用いた、エラーのない非同期処理の作成

---
+++ 03.asynchronous/callback-practice.js
@@ -0,0 +1,70 @@
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+const sqlite3 = require("sqlite3");
+const db = new sqlite3.Database(":memory:");
+
+db.run(
+  "create table books(id INTEGER PRIMARY KEY, title TEXT NOT NULL)",
+  (error) => {
+    if (error) {
+      console.error(error.message);
+    }
+
+    db.run("insert into books(title) values(?)", "ruby", (error) => {
+      if (error) {
+        console.error(error.message);
+      }
+
+      db.get("SELECT * FROM books WHERE title = ?", "ruby", (error, row) => {
+        if (error) {
+          console.error(error.message);
+        } else {
+          console.log(row.id);
+        }
+
+        db.run("insert into books(title) values(?)", "java", (error) => {
+          if (error) {
+            console.error(error.message);
+          }
+
+          db.get(
+            "SELECT * FROM books WHERE title = ?",
+            "java",
+            (error, row) => {
+              if (error) {
+                console.error(error.message);
+              } else {
+                console.log(row.id);
+              }
+
+              db.each(
+                "select * from books",
+                (error, row) => {
+                  if (error) {
+                    console.error(error.message);
+                  } else {
+                    console.log(`${row.id} ${row.title}`);
+                  }
+                },
+                (error) => {
+                  if (error) {
+                    console.error(error.message);
+                  }
+
+                  db.run("drop table books", (error) => {
+                    if (error) {
+                      console.error(error.message);
+                      return;
+                    }
+
+                    db.close();
+                  });
+                },
+              );
+            },
+          );
+        });
+      });
+    });
+  },
+);
