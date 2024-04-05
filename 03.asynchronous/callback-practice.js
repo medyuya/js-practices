aa45d2016316adc4e06ff5ff455289ccc541bd72
yuyanishimura <yuya.nishimura@medpeer.co.jp>
2024-04-05 17:41:28 +0900

    node-sqlite3のコールバック形式を用いた、エラーのある非同期処理の作成

--- 03.asynchronous/callback-practice.js
+++ 03.asynchronous/callback-practice.js
@@ -70,3 +70,81 @@ db.run(
 );
 
 await timers.setTimeout(300);
+
+console.error("------------");
+
+// エラー有り
+db.run(
+  "create table books(id INTEGER PRIMARY KEY, title TEXT NOT NULL)",
+  (error) => {
+    if (error) {
+      console.error(error.message);
+    }
+
+    db.run("insert into books(title) values(?)", "ruby", "swift", (error) => {
+      if (error) {
+        console.error(error.message);
+      }
+
+      db.get(
+        "SELECT * FROM books WHERE title = ?",
+        "python",
+        "C言語",
+        (error, row) => {
+          if (error) {
+            console.error(error.message);
+          } else {
+            console.log(row.id);
+          }
+
+          db.run(
+            "insert into books(title) values(?)",
+            "java",
+            "swift",
+            (error) => {
+              if (error) {
+                console.error(error.message);
+              }
+
+              db.get(
+                "SELECT * FROM books WHERE title = ?",
+                "javascript",
+                "typescript",
+                (error, row) => {
+                  if (error) {
+                    console.error(error.message);
+                  } else {
+                    console.log(row.id);
+                  }
+
+                  db.each(
+                    "select * from posts",
+                    (error, row) => {
+                      if (error) {
+                        console.error(error.message);
+                      } else {
+                        console.log(`${row.id} ${row.title}`);
+                      }
+                    },
+                    (error) => {
+                      if (error) {
+                        console.error(error.message);
+                      }
+
+                      db.run("drop table books", (error) => {
+                        if (error) {
+                          console.error(error.message);
+                          return;
+                        }
+                      });
+                    },
+                  );
+                },
+              );
+            },
+          );
+        },
+      );
+    });
+  },
+);
