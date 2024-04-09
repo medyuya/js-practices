8a0c658318bda960a2df41bb81244c66a8a535c3
yuyanishimura <yuya.nishimura@medpeer.co.jp>
2024-04-09 09:24:36 +0900

    コールバック形式のエラー無しプログラムにおける、不要なエラーハンドリングを削除

--- 03.asynchronous/callback-practice.js
+++ 03.asynchronous/callback-practice.js
@@ -8,58 +8,25 @@ const db = new sqlite3.Database(":memory:");
 // エラー無し
 db.run(
   "create table books(id INTEGER PRIMARY KEY, title TEXT NOT NULL)",
-  (error) => {
-    if (error) {
-      console.error(error.message);
-    }
-
-    db.run("insert into books(title) values(?)", "ruby", (error) => {
-      if (error) {
-        console.error(error.message);
-      }
-
+  () => {
+    db.run("insert into books(title) values(?)", "ruby", () => {
       db.get("SELECT * FROM books WHERE title = ?", "ruby", (error, row) => {
-        if (error) {
-          console.error(error.message);
-        } else {
-          console.log(row.id);
-        }
+        console.log(row.id);
 
-        db.run("insert into books(title) values(?)", "java", (error) => {
-          if (error) {
-            console.error(error.message);
-          }
-
+        db.run("insert into books(title) values(?)", "java", () => {
           db.get(
             "SELECT * FROM books WHERE title = ?",
             "java",
             (error, row) => {
-              if (error) {
-                console.error(error.message);
-              } else {
-                console.log(row.id);
-              }
+              console.log(row.id);
 
               db.each(
                 "select * from books",
                 (error, row) => {
-                  if (error) {
-                    console.error(error.message);
-                  } else {
-                    console.log(`${row.id} ${row.title}`);
-                  }
+                  console.log(`${row.id} ${row.title}`);
                 },
-                (error) => {
-                  if (error) {
-                    console.error(error.message);
-                  }
-
-                  db.run("drop table books", (error) => {
-                    if (error) {
-                      console.error(error.message);
-                      return;
-                    }
-                  });
+                () => {
+                  db.run("drop table books");
                 },
               );
             },
