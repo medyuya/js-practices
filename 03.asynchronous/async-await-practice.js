26b7bf3def5a042dd086c197ac68633bba47b905
yuyanishimura <yuya.nishimura@medpeer.co.jp>
2024-04-09 11:15:32 +0900

    必要なモジュールのインクルード

---
+++ 03.asynchronous/async-await-practice.js
@@ -0,0 +1,5 @@
+// ESMでCommonJSモジュールをインポートするため
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+const sqlite3 = require("sqlite3");
+const db = new sqlite3.Database(":memory:");
