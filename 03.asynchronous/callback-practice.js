026ab19cbc48c3ada94e5665f5ea93410ba445ac
yuyanishimura <yuya.nishimura@medpeer.co.jp>
2024-04-15 14:46:13 +0900

    sqlのパラメータに渡すデータの方を統一するため

--- 03.asynchronous/callback-practice.js
+++ 03.asynchronous/callback-practice.js
@@ -9,14 +9,18 @@ const db = new sqlite3.Database(":memory:");
 db.run(
   "CREATE TABLE books(id INTEGER PRIMARY KEY, title TEXT NOT NULL)",
   () => {
-    db.run("INSERT INTO books(title) VALUES(?)", "ruby", function () {
+    db.run("INSERT INTO books(title) VALUES(?)", ["ruby"], function () {
       console.log(this.lastID);
 
-      db.get("SELECT * FROM books WHERE title = ?", "ruby", function (_, row) {
-        console.log(row.id);
+      db.get(
+        "SELECT * FROM books WHERE title = ?",
+        ["ruby"],
+        function (_, row) {
+          console.log(row.id);
 
-        db.run("DROP TABLE books");
-      });
+          db.run("DROP TABLE books");
+        },
+      );
     });
   },
 );
