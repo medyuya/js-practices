6f262479266411b19e20d9109c2593ae50389712
yuyanishimura <yuya.nishimura@medpeer.co.jp>
2024-04-18 11:14:10 +0900

    カレンダーの表示を行ごとではなく、全体で一つのテキストとして扱うように変更するため

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -25,6 +25,7 @@ const args = minimist(process.argv.slice(2));
 const today = new Date();
 const targetYear = args.y ?? today.getFullYear();
 const targetMonth = args.m ?? today.getMonth() + 1;
+
 const startDate = new Date(targetYear, targetMonth - 1, 1);
 const endDate = new Date(targetYear, targetMonth, 0);
 
@@ -34,27 +35,23 @@ const TOTAL_ROW_LENGTH = 20;
 const monthYearTitle = `${targetMonthName} ${targetYear}`;
 const centeredMonthYearTitle = centralizeText(TOTAL_ROW_LENGTH, monthYearTitle);
 
-console.log(centeredMonthYearTitle);
-console.log("Su Mo Tu We Th Fr Sa");
-
-let weekRowText = " ".repeat(dfns.getDay(startDate) * 3);
+let resultCalendarText = `${centeredMonthYearTitle}\n`;
+resultCalendarText += "Su Mo Tu We Th Fr Sa\n";
+resultCalendarText += " ".repeat(dfns.getDay(startDate) * 3);
 
 for (
   const date = new Date(targetYear, targetMonth - 1, 1);
   date <= endDate;
   date.setDate(date.getDate() + 1)
 ) {
-  weekRowText += `${date
+  resultCalendarText += `${date
     .getDate()
     .toString()
     .padStart(2, " ")}${insertSpaceExceptForLineEnd(date)}`;
 
-  if (
-    dfns.isSaturday(date) ||
-    (date.getDate() === endDate.getDate() &&
-      date.getMonth() === endDate.getMonth())
-  ) {
-    console.log(weekRowText);
-    weekRowText = "";
+  if (dfns.isSaturday(date)) {
+    resultCalendarText += "\n";
   }
 }
+
+console.log(resultCalendarText);
